4e5af6b6fbde2325bd7b8cdb3e03de09bd164796
이용재 <dididy@knou.ac.kr>
2019-06-28 02:15:39 +0900

    Design work enhanced

--- frontend/src/layouts/VuebaseLayout.vue
+++ frontend/src/layouts/VuebaseLayout.vue
@@ -13,16 +13,17 @@
             >
                 <v-spacer></v-spacer>
                 <div class="v-toolbar-title">
-                    <router-link to="/"><h2 style="color:#d8d8d8;text-decoration:none">족보.io</h2></router-link>
+                    <router-link to="/"><h2 style="color:#d8d8d8;text-decoration:none">Jokjebi</h2></router-link>
                 </div>
                 <v-spacer></v-spacer>
+                    <div class="hideit" style="padding-right:40px">
+                        <router-link to="/jockbolist"><h2 style="color:#7d7d7d;font-size:15px"><b>스크랩한 족보</b></h2>
+                        </router-link>
+                    </div>
 
-                <div style="padding-right:40px">
-                    <router-link to="/jockbolist"><h2 style="color:#7d7d7d;font-size:15px"><b>스크랩한 족보</b></h2></router-link>
-                </div>
-
-                <div style="padding-right:70px">
-                    <router-link to="/createjockbo"><h2 style="color:#7d7d7d;font-size:15px"><b>족보 업로드</b></h2></router-link>
+                    <div class="hideit" style="padding-right:70px">
+                        <router-link to="/createjockbo"><h2 style="color:#7d7d7d;font-size:15px"><b>족보 업로드</b></h2>
+                        </router-link>
                 </div>
                 <div style="  border-left: 1px solid #e5e5e5; height: 70px;"></div>
                 <div style="padding-left:20px;padding-right:40px">
@@ -213,14 +214,14 @@
                 this.$store.commit('changeAuthModalState');
             },
             closeAuthModal(event) {
-                if(this.$store.state.authModalState === true)
+                if (this.$store.state.authModalState === true)
                     this.$store.commit('changeAuthModalState')
             },
             blockPropagate(event) {
                 event.stopPropagation();
             },
             login() {
-                this.$store.dispatch('obtainToken', {username:this.username, password:this.password});
+                this.$store.dispatch('obtainToken', {username: this.username, password: this.password});
                 // axios({
                 //     method: 'post',
                 //     url: 'http://127.0.0.1:8000/signin/',
@@ -239,7 +240,7 @@
                 //     })
                 // }).catch(function (response) {
                 //     console.log(response);
-                // });                
+                // });
             }
         }
     }
@@ -247,7 +248,16 @@
 
 <style scoped lang="stylus">
     @import '../../node_modules/vuetify/src/stylus/settings/_variables.styl';
-    a {  text-decoration: none;}
+    a {
+        text-decoration: none;
+    }
+
+    @media screen and (max-width: 960px) {
+        .hideit {
+            display: none;
+        }
+
+    }
 
     .bottom-menu {
         position: absolute;
@@ -295,7 +305,7 @@
 
     .auth-modal {
         display: none;
-        
+
         position: fixed;
         top: 0;
         left: 0;
