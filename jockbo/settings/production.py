a534dcbbd22ffa4f78f99a2780a743e82986459c
DONGWON KIM <lunacircle4@gmail.com>
2019-07-17 03:26:03 +0900

    production 시도

--- jockbo/settings/production.py
+++ jockbo/settings/production.py
@@ -1,5 +1,6 @@
 from jockbo.settings.base import *
 import dj_database_url, os
+
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 DEBUG = False
@@ -7,11 +8,6 @@ DEBUG = False
 ALLOWED_HOSTS = ['.herokuapp.com']
 
 CORS_ORIGIN_ALLOW_ALL = True
-
-DATABASES = {
-    'default': {
-    }
-}
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
